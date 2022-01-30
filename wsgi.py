6d07c6021bdfd705f08db1a810c4f5cde7d4b39b
Derrick Wong <darkwing2025@gmail.com>
2022-01-30 22:43:25 +0800

    editing wsgi server

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,10 @@ CODE_DIR = abspath(join(THIS_DIR, 'app'))
 sys.path.append(CODE_DIR)
 print(CODE_DIR)
 
-if __name__ == '__main__':
-    from gevent.pywsgi import WSGIServer
-    from cii import app
+from gevent.pywsgi import WSGIServer
+from cii import app
 
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    
+ http_server = WSGIServer(('', 5000), app)
+ http_server.serve_forever()
     
